95fb5354b317acf60ad75ef0a97daebf096b6cf3
KhalitovRA <halitov-02@mail.ru>
2021-12-08 22:19:04 +0300

    work with reviews

--- django_movie/movies/serializers.py
+++ django_movie/movies/serializers.py
@@ -1,6 +1,18 @@
 from rest_framework import serializers
 
-from .models import Movie
+from .models import Movie, Review
+
+
+class FilterReviewListSerializer(serializers.ListSerializer):
+    def to_representation(self, data):
+        data = data.filter(parent=None)
+        return super().to_representation(data)
+
+class RecursiveSerializer(serializers.Serializer):
+
+    def to_representation(self, value):
+        serializer = self.parent.parent.__class__(value, context=self.context)
+        return serializer.data
 
 
 class MovieListSerializer(serializers.ModelSerializer):
@@ -10,11 +22,28 @@ class MovieListSerializer(serializers.ModelSerializer):
         fields = ('title', 'tagline', 'category')
 
 
+class ReviewCreateSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Review
+        fields = '__all__'
+
+
+class ReviewSerializer(serializers.ModelSerializer):
+    children = RecursiveSerializer(many=True)
+
+    class Meta:
+        list_serializer_class = FilterReviewListSerializer
+        model = Review
+        fields = ('name', 'text', 'children')
+
+
 class MovieDetailSerializer(serializers.ModelSerializer):
     category = serializers.SlugRelatedField(slug_field='name', read_only=True)
     directors = serializers.SlugRelatedField(slug_field='name', read_only=True, many=True)
     actors = serializers.SlugRelatedField(slug_field='name', read_only=True, many=True)
     genres = serializers.SlugRelatedField(slug_field='name', read_only=True, many=True)
+    reviews = ReviewSerializer(many=True)
 
     class Meta:
         model = Movie
